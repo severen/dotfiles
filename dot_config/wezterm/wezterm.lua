cea8a1d520b20daac96bb2462642c5a65294e7e6
Severen Redwood <me@severen.dev>
2023-11-22 13:55:11 +1300

    Correct font name in Wezterm config

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,22 +1,14 @@
 local wezterm = require("wezterm")
 
-function scheme_for_appearance(appearance)
-  if appearance:find("Dark") then
-    return "Catppuccin Mocha"
-  else
-    return "Catppuccin Latte"
-  end
-end
-
 return {
   font = wezterm.font_with_fallback({
-    "JetBrains Mono Nerd Font",
+    "JetBrains Mono NF",
     "JetBrains Mono",
     "Noto Color Emoji",
   }),
   harfbuzz_features = { "zero" },
 
-  color_scheme = scheme_for_appearance(wezterm.gui.get_appearance()),
+  color_scheme = "Catppuccin Mocha",
   window_background_opacity = 0.90,
 
   enable_scroll_bar = true,
