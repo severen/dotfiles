72c93cfaadc3a02111c31428714c667e3fd9ac65
Severen Redwood <sev@severen.dev>
2024-12-29 16:50:17 +1300

    Remove transparent background autocmd for Neovim

--- dot_config/nvim/lua/config.lua
+++ dot_config/nvim/lua/config.lua
@@ -261,14 +261,6 @@ require("lazy").setup({
 -- Use 24-bit colour like it's 1995!
 opt.termguicolors = true
 
--- Make the background transparent.
-api.nvim_create_autocmd("ColorScheme", {
-  callback = function()
-    cmd.highlight("Normal ctermbg=none guibg=none")
-    cmd.highlight("NonText ctermbg=none guibg=none")
-  end,
-})
-
 -- Load my default colorscheme of choice.
 cmd.colorscheme("catppuccin-mocha")
 
