5527559177e35ac86f7b3636f8eda1094eb5e78a
Severen Redwood <sev@severen.dev>
2025-01-16 19:29:14 +1300

    Update Tree-Sitter parsers when Neovim plugin is updated

--- dot_config/nvim/lua/config.lua
+++ dot_config/nvim/lua/config.lua
@@ -166,6 +166,9 @@ require("lazy").setup({
   -- {{{ Language Support
   {
     "nvim-treesitter/nvim-treesitter",
+    build = function()
+      require("nvim-treesitter.install").update({ with_sync = true })()
+    end,
     config = function()
       require("nvim-treesitter.configs").setup({
         ensure_installed = {
