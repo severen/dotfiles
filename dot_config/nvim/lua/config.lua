5dbee38f042cac5c66cfa093f0b19a0cbf4a0129
Severen Redwood <sev@severen.dev>
2025-01-16 19:29:14 +1300

    nvim: remove cmp plugin

--- dot_config/nvim/lua/config.lua
+++ dot_config/nvim/lua/config.lua
@@ -147,21 +147,7 @@ require("lazy").setup({
   -- }}}
 
   -- {{{ Completion and Diagnostics
-  {
-    "hrsh7th/nvim-cmp",
-    name = "cmp",
-    -- load cmp on InsertEnter
-    event = "InsertEnter",
-    -- these dependencies will only be loaded when cmp loads
-    -- dependencies are always lazy-loaded unless specified otherwise
-    dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-buffer",
-    },
-    config = function()
-      -- TODO: Setup completion.
-    end,
-  },
+  -- TODO: Set up completion.
 
   -- {{{ Language Support
   {
