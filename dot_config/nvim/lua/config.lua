4d2f8eb26c8380aa80cccda43ae2eac04c8337af
Severen Redwood <sev@severen.dev>
2025-04-03 01:27:55 +1300

    nvim: set up snacks.nvim plugin

--- dot_config/nvim/lua/config.lua
+++ dot_config/nvim/lua/config.lua
@@ -85,9 +85,141 @@ require("lazy").setup({
 
   -- Interface ⦃
   {
+    "folke/persistence.nvim",
+    event = "BufReadPre", -- this will only start session saving when an actual file was opened
+    opts = {},
+  },
+  {
     "folke/which-key.nvim",
     name = "which-key",
     config = true,
+  },
+  {
+    "folke/snacks.nvim",
+    dependencies = { "echasnovski/mini.nvim", version = false },
+    priority = 1000,
+    lazy = false,
+    opts = {
+      dashboard = { enabled = true },
+      explorer = { enabled = true },
+      input = { enabled = true },
+      picker = { enabled = true },
+      session = { enabled = true },
+      zen = { enabled = true },
+    },
+    keys = {
+      {
+        "<leader><leader>",
+        function()
+          Snacks.picker.smart()
+        end,
+        desc = "Smart Find Files",
+      },
+      {
+        "<leader>,",
+        function()
+          Snacks.picker.buffers()
+        end,
+        desc = "Buffers",
+      },
+      {
+        "<leader>/",
+        function()
+          Snacks.picker.grep()
+        end,
+        desc = "Grep",
+      },
+      {
+        "<leader>:",
+        function()
+          Snacks.picker.command_history()
+        end,
+        desc = "Command History",
+      },
+      {
+        "<leader>n",
+        function()
+          Snacks.picker.notifications()
+        end,
+        desc = "Notification History",
+      },
+      {
+        "<leader>e",
+        function()
+          Snacks.explorer()
+        end,
+        desc = "File Explorer",
+      },
+
+      {
+        "<leader>fb",
+        function()
+          Snacks.picker.buffers()
+        end,
+        desc = "Buffers",
+      },
+      {
+        "<leader>fc",
+        function()
+          Snacks.picker.files({ cwd = vim.fn.stdpath("config") })
+        end,
+        desc = "Find Config File",
+      },
+      {
+        "<leader>ff",
+        function()
+          Snacks.picker.files()
+        end,
+        desc = "Find Files",
+      },
+      {
+        "<leader>fg",
+        function()
+          Snacks.picker.git_files()
+        end,
+        desc = "Find Git Files",
+      },
+      {
+        "<leader>fp",
+        function()
+          Snacks.picker.projects()
+        end,
+        desc = "Projects",
+      },
+      {
+        "<leader>fr",
+        function()
+          Snacks.picker.recent()
+        end,
+        desc = "Recent",
+      },
+
+      {
+        "<leader>z",
+        function()
+          Snacks.zen()
+        end,
+        desc = "Toggle Zen Mode",
+      },
+      {
+        "<leader>N",
+        desc = "Neovim News",
+        function()
+          Snacks.win({
+            file = vim.api.nvim_get_runtime_file("doc/news.txt", false)[1],
+            width = 0.9,
+            height = 0.8,
+            wo = {
+              spell = false,
+              wrap = false,
+              signcolumn = "yes",
+              statuscolumn = " ",
+              conceallevel = 3,
+            },
+          })
+        end,
+      },
+    },
   },
   {
     "mikavilpas/yazi.nvim",
@@ -295,7 +427,6 @@ vim.opt.guicursor = {
 
 -- Interface ⦃
 
-map("n", "<leader><leader>", ":", { desc = "Run Ex command" })
 map("n", "<leader>q", ":qa<CR>", { desc = "Quit" })
 map("n", "<leader>Q", ":qa<CR>", { desc = "Force quit" })
 
